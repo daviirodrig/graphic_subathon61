75af6fe018a2f9080a947935489257428a78c5e9
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2024-08-17 21:58:00 -0300

    add some files and new format to read from json

--- main.py
+++ main.py
@@ -1,53 +1,83 @@
+from typing import Generator
+from zoneinfo import ZoneInfo
 import matplotlib.pyplot as plt
-import requests
+from datetime import datetime
 import numpy as np
-from time import sleep, strftime
+
 import os
 import shutil
 import sys
+import json
+import matplotx
 
 timeList = []
 x = 0
 
-folder = 'images'
+folder = "images"
 
 if os.path.exists(folder):
-    choice_delete = input('do you want to delete the folder and continue? (yes or no): ')
-    if choice_delete == 'yes':
-        print('ok! deleting the folder...')
-        shutil.rmtree(folder, ignore_errors=False, onerror=None)
+    choice_delete = input(
+        "do you want to delete the folder and continue? (yes or no): "
+    )
+    if choice_delete == "yes":
+        print("ok! deleting the folder...")
+        shutil.rmtree(folder, ignore_errors=False)
         os.makedirs(folder)
-        print('program starting!')
-    if choice_delete == 'no':
-        print('ok! stopping now.')
+        print("program starting!")
+    if choice_delete == "no":
+        print("ok! stopping now.")
         sys.exit()
+else:
+    os.makedirs(folder)
 
-def getTime():
-    try:
-        url = 'https://subathon-api.justdavi.dev/api/time-left'
-        headers = {
-            'User-Agent': 'graph-lexpdev-xyz'
-        }
-        responseTL = requests.get(url, headers=headers)
-        responseTL.raise_for_status()
-        milissecondsLeft = int(responseTL.json()['timeLeft'])
-        hoursLeft = milissecondsLeft / 3600000
-        return round(hoursLeft, 3)
-    except:
-        return 'error'
 
-while True:
-    time = getTime()
-    if not time == 'error':
-        print(f'{time}, time {x}')
-        timeList.insert(x, time)
-        ypoints = np.array(timeList)
-        x = x + 1
-        plt.plot(ypoints, color='black')
-        current_time = strftime("%H:%M:%S")
-        plt.text(0.95, 0.95, current_time, transform=plt.gca().transAxes,
-                 fontsize=12, verticalalignment='top', horizontalalignment='right')
-        
-        plt.savefig(f'{folder}/graph_{x}.png')
+def getTime() -> Generator[tuple[float, str], None, None]:
+    with open("interpolated.json") as f:
+        data = json.load(f)
+        for i in data:
+            yield i["timer"], i["timestamp"]
+
+
+times = getTime()
+for i in times:
+    timer, current_time = i
+    timer_h = int(timer / 3600000)
+    timer_m = int((timer - timer_h * 3600000) / 60000)
+    timer_s = int((timer - timer_h * 3600000 - timer_m * 60000) / 1000)
+    current_time = (
+        datetime.fromisoformat(current_time)
+        .astimezone(ZoneInfo("America/Sao_Paulo"))
+        .strftime("%d/%m %H:%M:%S")
+    )
+    timer_str = f"{timer_h:03}:{timer_m:02}:{timer_s:02}"
+
+    print(f"{timer_h}, {x}")
+
+    timeList.insert(x, timer_h)
+    ypoints = np.array(timeList)
+    x = x + 1
+    with plt.style.context(matplotx.styles.duftify(matplotx.styles.github["dark"])):
+        plt.plot(ypoints)
+        plt.text(
+            x=0.25,
+            y=1.05,
+            s=f"{current_time}",
+            transform=plt.gca().transAxes,
+            fontsize=14,
+            verticalalignment="top",
+            horizontalalignment="center",
+            color="white",
+        )
+        plt.text(
+            0.75,
+            1.05,
+            f"{timer_str}",
+            transform=plt.gca().transAxes,
+            fontsize=14,
+            verticalalignment="top",
+            horizontalalignment="center",
+            color="white",
+        )
+
+        plt.savefig(f"{folder}/graph_{x}.png")
         plt.clf()
-    sleep(30)
